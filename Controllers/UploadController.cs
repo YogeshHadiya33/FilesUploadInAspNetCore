0bd9401a0bda520bd0c88d06aabfa22c1f38769b
YogeshHadiya <hadiyayk33@gmail.com>
2022-02-08 18:17:13 +0530

    Add project files.

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UploadFile.Models;
+
+namespace UploadFile.Controllers
+{
+    public class UploadController : Controller
+    {
+        public IActionResult Index()
+        {
+            SingleFileModel model = new SingleFileModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Upload(SingleFileModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.IsResponse = true;
+
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Files");
+
+                //create folder if not exist
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+
+                //get file extension
+                FileInfo fileInfo = new FileInfo(model.File.FileName);
+                string fileName = model.FileName + fileInfo.Extension;
+
+                string fileNameWithPath = Path.Combine(path, fileName);
+
+                using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                {
+                    model.File.CopyTo(stream);
+                }
+
+                model.IsSuccess = true;
+                model.Message = "File upload successfully";
+
+            }
+            return View("Index", model);
+        }
+
+        public IActionResult MultiFile()
+        {
+            MultipleFilesModel model = new MultipleFilesModel();
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public IActionResult MultiUpload(MultipleFilesModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.IsResponse = true;
+                if (model.Files.Count > 0)
+                {
+                    foreach (var file in model.Files)
+                    {
+
+                        string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Files");
+
+                        //create folder if not exist
+                        if (!Directory.Exists(path))
+                            Directory.CreateDirectory(path);
+
+
+                        string fileNameWithPath = Path.Combine(path, file.FileName);
+
+                        using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                        {
+                            file.CopyTo(stream);
+                        }
+
+                    }
+                    model.IsSuccess = true;
+                    model.Message = "Files upload successfully";
+                }
+                else
+                {
+                    model.IsSuccess = false;
+                    model.Message = "Please select files";
+                }
+            }
+
+            return View("MultiFile", model);
+        }
+
+
+    }
+}
